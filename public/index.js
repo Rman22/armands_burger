1e419a44ff097db2cec2ce9bc7f34fcb4a55f1be
Rman22 <rmanmekini@gmail.com>
2021-09-24 18:10:32 +0800

    update

--- public/index.js
+++ public/index.js
@@ -207,7 +207,7 @@ function addMenus(data){
                         
                         <div class="bg-white px-3 py-2  ">
                             <h2 class="text-lg text-gray-700 font-semibold">${data[i].burgerName}</h2>
-                            <p>₱${data[i].burgerInclusion}</p>
+                            <p>₱+${data[i].burgerInclusion}</p>
                             
                         </div>
                         <div class="cartDiv flex justify-between bg-pallete6 rounded-b-lg px-5">
@@ -238,7 +238,7 @@ function addMenus(data){
                                 
                             </div>
                             <div class="cartDiv flex justify-between bg-pallete6 rounded-b-lg px-5">
-                                <span class="py-2 text-pallete3 flex justify-center  ">${data[i].burgerPrice}</span>
+                                <span class="py-2 text-pallete3 flex justify-center">₱${data[i].burgerPrice}</span>
                                 <button class="cartBtn text-pallete3" id="addToCartBtn"><svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
                                     <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M3 3h2l.4 2M7 13h10l4-8H5.4M7 13L5.4 5M7 13l-2.293 2.293c-.63.63-.184 1.707.707 1.707H17m0 0a2 2 0 100 4 2 2 0 000-4zm-8 2a2 2 0 11-4 0 2 2 0 014 0z" />
                                 </svg></button>
