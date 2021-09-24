c87bdf45802fd33c73bab14586956e1799414eab
Rman22 <rmanmekini@gmail.com>
2021-09-24 19:35:25 +0800

    update

--- public/animation.js
+++ public/animation.js
@@ -1,12 +1,13 @@
-// anime({
-//         targets: '.homeImage',
-//         translateX: 500,
-//         direction: 'reverse',
-//         easing: 'linear',
-//         duration: 1400,
+
+anime({
+        targets: '.homeImage',
+        translateX: 500,
+        direction: 'reverse',
+        easing: 'linear',
+        duration: 1400,
 
     
-//       });
+      });
 window.sr = ScrollReveal();
     sr.reveal('.aboutBurger',{
     duration: 1000,
